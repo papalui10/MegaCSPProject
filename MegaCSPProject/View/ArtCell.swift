3eebd97ada4091ca16bb2f94c9d374913c3edffc
Luis Veliz <luisvelizjr@gmail.com>
2019-01-08 12:31:44 -0700

    added all the storyboards from the other project to this one and added the pictures from those same projects to assets.xcassets

---
+++ MegaCSPProject/View/ArtCell.swift
@@ -0,0 +1,16 @@
+//
+//  ArtCellCollectionViewCell.swift
+//  ArtProject
+//
+//  Created by Veliz, Luis on 11/7/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class ArtCell: UICollectionViewCell
+{
+    @IBOutlet weak var artImage: UIImageView!
+    
+    @IBOutlet weak var artLabel: UILabel!
+}
---
+++ MegaCSPProject/View/ArtView.swift
@@ -0,0 +1,75 @@
+//
+//  ArtView.swift
+//  ArtProject
+//
+//  Created by Veliz, Luis on 11/1/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+ public class ArtView: UIView
+ {
+
+    
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+   public override func draw(_ rect: CGRect) -> Void
+   {
+        // Drawing code
+        createStickFigure().stroke()
+        drawTurtle()
+        drawSquare()
+    }
+    
+    private func createStickFigure() -> UIBezierPath
+    {
+        let figure : UIBezierPath = UIBezierPath()
+        
+        UIColor.magenta.setStroke()
+        figure.lineWidth = 3.0
+        
+        figure.addArc(withCenter: CGPoint(x: 200, y: 200),
+                      radius: CGFloat(20),
+                      startAngle: CGFloat(0),
+                      endAngle: CGFloat(2) * CGFloat.pi,
+                      clockwise: true)
+        
+        figure.move(to: CGPoint(x: 200, y: 220))
+        figure.addLine(to: CGPoint(x: 200, y: 270))
+        figure.move(to: CGPoint(x: 180, y: 240))
+        figure.addLine(to: CGPoint(x: 220, y: 240))
+        figure.move(to: CGPoint(x: 200, y: 270))
+        figure.addLine(to: CGPoint(x: 180, y: 300))
+        figure.move(to: CGPoint(x: 200, y: 270))
+        figure.addLine(to: CGPoint(x: 220, y: 300))
+        
+        
+        return figure
+    }
+    
+    private func drawTurtle() -> Void
+    {
+        let logo = UIBezierPath()
+        UIColor.white.setFill()
+        logo.move(to: CGPoint(x: 50, y: 250))
+        logo.addLine(to: CGPoint(x: 100, y: 300))
+        logo.addLine(to: CGPoint(x: 50, y: 350))
+        logo.close()
+        logo.fill()
+    }
+    
+    private func drawSquare() -> Void
+    {
+        let line = UIBezierPath()
+        UIColor.white.setFill()
+        line.move(to: CGPoint(x: 20, y: 20))
+        line.addLine(to: CGPoint(x: 40, y: 20))
+        line.addLine(to: CGPoint(x: 40, y: 40))
+        line.addLine(to: CGPoint(x: 20, y: 40))
+        line.addLine(to: CGPoint(x: 20, y: 20))
+        
+        line.fill()
+    }
+
+}
